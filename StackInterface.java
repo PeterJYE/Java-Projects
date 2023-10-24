ca0a751b02bd3ec0b6981c85147d5e0b5245055e
PeterJYE <148128006+PeterJYE@users.noreply.github.com>
2023-10-24 13:49:30 -0400

    Add files via upload

---
+++ StackInterface.java
@@ -0,0 +1,14 @@
+package support;
+
+public interface StackInterface<E> {
+	E pop() throws StackUnderflowException;
+	
+	E top() throws StackUnderflowException;
+	
+	void push (E element);
+	
+	boolean isEmpty();
+	
+	String toString();
+
+}
---
+++ StackUnderflowException.java
@@ -0,0 +1,12 @@
+package support;
+
+public class StackUnderflowException extends Exception{
+
+	 public StackUnderflowException(){
+         super();
+     }
+
+     public StackUnderflowException(String message){
+          super(message); 
+     }
+}
